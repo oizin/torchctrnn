d93a7dcd80c43f22e5e5f9770cd9828ffa00471c
Oisin Fitzgerald <o.fitzgerald@unsw.edu.au>
2022-05-11 15:21:02 +1000

    draft 1 of glucose simulation

---
+++ tests/test_simulations.py
@@ -0,0 +1,19 @@
+import unittest
+import torch
+import torch.nn as nn
+from torchctrnn.data.simulations import GlucoseData
+
+class TestODENet(unittest.TestCase):
+
+    def test_glucose(self):
+        sim1 = GlucoseData(seed=1234)
+        df1 = sim1.simulate(1)
+        df2 = sim1.simulate(1)
+        sim2 = GlucoseData(seed=1234)
+        df3 = sim2.simulate(1)
+
+        self.assertEqual(df1.glucose_t[0], df2.glucose_t[0])
+        self.assertEqual(df1.glucose_t[0], df3.glucose_t[0])
+
+if __name__ == '__main__':
+    unittest.main()
--- torchctrnn/data/simulations.py
+++ torchctrnn/data/simulations.py
@@ -1,29 +1,32 @@
-from scipy.stats import norm,lognorm 
 import numpy as np
 import pandas as pd
 import random
-from typing import Callable
+import math
+from numba import jit
+import typing
 
-def next_observation_time(glucose,insulin_dose):
+@jit(nopython=True)
+def _temporal_process(glucose:float,insulin_dose:float):
     """
     Next observation time
     """
     # insulin being used?
     if insulin_dose > 0:
-        base = 3
+        base = 3.0
     else:
-        base = 5
+        base = 5.0
     # measured glucose
-    if glucose < 80:
-        mean = base * np.exp(-((glucose - 120)/50) ** 2)
+    if glucose < 80.0:
+        mean = base * np.exp(-((glucose - 120.0)/50.0) ** 2.0)
     else: 
-        mean = base * np.exp(-((glucose - 120)/140) ** 2)
+        mean = base * np.exp(-((glucose - 120.0)/140.0) ** 2.0)
     # some randomness in times
-    log_time = norm.rvs(loc=np.log(mean),scale=0.1)
+    log_time = np.random.normal(np.log(mean),0.1)
     time = np.exp(log_time)
     return time
 
-def insulin_input(glucose):
+@jit(nopython=True)
+def _insulin_policy(glucose:float,insulin_dose:float=0.0):
     """
     dose over an hour
     """
@@ -37,103 +40,119 @@ def insulin_input(glucose):
         dose_hr = 30.0
     return dose_hr / 60.0
 
-def dextrose_input(t):
-
-    if random.random() > 0.9:
-        glucose_mg_min = 180.0 + round(random.random()*10) 
+@jit(nopython=True)
+def _dextrose_policy(t:float):
+    """
+    """
+    if np.random.random() > 0.9:
+        glucose_mg_min = 180.0 + np.random.random()*10.0
     else: 
         glucose_mg_min = 0.0
     return glucose_mg_min / 50.0
 
+@jit(nopython=True)
+def _simulate_trajectory(sigma_m:float=0.0):
+
+    dt = 0.01
+    maxtime = 24.0
+    n_iter = int(maxtime / dt) + 1
+    saveat = range(0,n_iter,int(0.1 / dt))
+
+    # output container
+    column_name_pos = {'t':0,
+                       'glucose_t':1,
+                       'glucose_t_obs':2,
+                       'obs':3,
+                       'insulin_t':4,
+                       'dextrose_t':5}
+    output = np.zeros((len(saveat), len(column_name_pos)))
+    output[:,column_name_pos['t']] = np.linspace(0,maxtime,len(saveat))
+
+    # set parameters
+    beta = -np.random.normal(50,5)
+    sigma = np.random.normal(20.0,2)
+    mu = np.random.normal(140,5)
+    theta = np.random.normal(0.5,0.01)
+    
+    # time 0
+    insulin_t = 0.0
+    glucose_t = np.random.normal(140,20)
+    glucose_t_obs = np.exp(np.random.normal(np.log(glucose_t),sigma_m))
+    iter = 0
+    save_step = 0
+    iter_measure = 0
+    obs = False
+
+    ## loop through time steps
+    while iter <= n_iter:
+        if iter == iter_measure:
+            obs = True
+            dextrose_t = _dextrose_policy(iter * dt)
+            insulin_t = _insulin_policy(glucose_t,insulin_t)
+            delta_t = _temporal_process(glucose_t,insulin_t)
+            iter_measure = min(iter + int(np.round(delta_t/dt,-1)),n_iter)
+            
+        else:
+            obs = False
+
+        dW = np.random.normal(0,dt)
+        d_glucose_deter = (theta*(mu - glucose_t) + beta*insulin_t + dextrose_t)*dt 
+        d_glucose_stoch = np.sqrt(2*theta*sigma**2)*dW
+        d_glucose = d_glucose_deter + d_glucose_stoch
+        glucose_t = glucose_t + d_glucose
+        glucose_t_obs = np.exp(np.random.normal(np.log(glucose_t),sigma_m))
+
+        if iter in saveat:
+            output[save_step,column_name_pos['glucose_t']] = glucose_t
+            output[save_step,column_name_pos['glucose_t_obs']] = glucose_t_obs
+            output[save_step,column_name_pos['obs']] = obs
+            output[save_step,column_name_pos['insulin_t']] = insulin_t
+            output[save_step,column_name_pos['dextrose_t']] = dextrose_t 
+            save_step += 1
+        iter += 1
+
+    return output
+
+@jit(nopython=True)
+def _simulate(N:int,sigm_m:float=0.0,seed:int=None):
+
+    np.random.seed(seed)
+
+    # output container
+    n_iter = int(24.0 / 1e-2) + 1
+    saveat = range(0,n_iter,int(0.1 / 1e-2))
+    n_save_steps = len(saveat)
+    output = np.zeros((n_save_steps*N, 6))
+
+    for i in range(N):
+        output_i = _simulate_trajectory(sigm_m)
+        lower = i*n_save_steps
+        upper = (i+1)*n_save_steps
+        output[lower:upper,:] = output_i
+
+    return output
+
 
 class GlucoseData:
     
-    def __init__(self,
-                insulin_policy:Callable=insulin_input,
-                dextrose_policy:Callable=dextrose_input,
-                temporal_process:Callable=next_observation_time,
-                measurement_error:float=0.0):
+    def __init__(self,measurement_error:float=0.0,seed=None):
         """
         Args:
-            insulin_policy
-            dextrose_policy
-            temporal_process
             measurement_error
         """
         # arguments
-        self.insulin_policy = insulin_policy
-        self.dextrose_policy = dextrose_policy
-        self.temporal_process = temporal_process
         self.sigma_m = measurement_error
+        self.seed = seed
 
-        # 
-        self.dt = 1e-2
+        # other
+        self.dt = 0.01
         self.maxtime = 24.0
+        self.columns = ['t','glucose_t','glucose_t_obs',
+                       'obs','insulin_t','dextrose_t']
 
-
-    def simulate_trajectory(self):
-
-        # random variables
-        p_theta = norm(0.5,0.01)  # mean reversion strength of process
-        p_sigma = norm(20.0,2)  # variance of process
-        p_mu = norm(140,5)  # mean of process
-        p_t0 = norm(140,20)  # initial value
-        p_beta = norm(50,5)  # insulin effect
-
-        # time
-        dt = self.dt
-        maxtime = self.maxtime
-        n_iter = int(maxtime / dt) + 1
-        saveat = range(0,n_iter,int(0.1 / dt))
-
-        # output container
-        output = pd.DataFrame(np.zeros((len(saveat), 5)),
-                                columns=['glucose_t','glucose_t_obs',
-                                        'obs','insulin_t','dextrose_t'])
-        output['t'] = np.linspace(0,maxtime,len(saveat))
-
-        # set parameters
-        beta = -p_beta.rvs(1)
-        sigma = p_sigma.rvs(1)
-        mu = p_mu.rvs(1)
-        theta = p_theta.rvs(1)
-        
-        # time 0
-        glucose_t = glucose_t0 = p_t0.rvs(1)
-        glucose_t_obs = glucose_t0  + self.sigma_m * norm.rvs(1) * np.log(glucose_t0)
-        iter = 0
-        save_step = 0
-        iter_measure = 0
-        obs = False
-
-        ## loop through time steps
-        while iter <= n_iter:
-            if iter == iter_measure:
-                obs = True
-                dextrose_t = self.dextrose_policy(iter * 1e-2)
-                insulin_t = self.insulin_policy(glucose_t)
-                delta_t = self.temporal_process(glucose_t,insulin_t)
-                iter_measure = min(iter + int(round(delta_t/dt,-1)),n_iter)
-            else:
-                obs = False
-
-            dW = norm.rvs(0,dt,1)
-            d_glucose_deter = (theta*(mu - glucose_t) + beta*insulin_t + dextrose_t)*dt 
-            d_glucose_stoch = np.sqrt(2*theta*sigma**2)*dW
-            d_glucose = d_glucose_deter + d_glucose_stoch
-            glucose_t = glucose_t + d_glucose
-            glucose_t_obs = glucose_t + self.sigma_m * norm.rvs(size=1) * np.log(glucose_t)
-
-            if iter in saveat:
-                output.loc[save_step,'glucose_t'] = glucose_t
-                output.loc[save_step,'glucose_t_obs'] = glucose_t_obs
-                output.loc[save_step,'obs'] = obs
-                output.loc[save_step,'insulin_t'] = insulin_t
-                output.loc[save_step,'dextrose_t'] = dextrose_t 
-                save_step += 1
-            iter += 1
-
-        return output
+    # def simulate_trajectory(self):
+    #     output = _simulate_trajectory(self.sigma_m)
+    #     return output
 
     def simulate(self,N:int=100):
 
@@ -141,40 +160,17 @@ class GlucoseData:
         n_iter = int(self.maxtime / self.dt) + 1
         saveat = range(0,n_iter,int(0.1 / self.dt))
         n_save_steps = len(saveat)
-        output = pd.DataFrame(np.zeros((n_save_steps*N, 5)),
-                                columns=['glucose_t','glucose_t_obs',
-                                        'obs','insulin_t','dextrose_t'])
-        output['t'] = np.repeat(np.linspace(0,self.maxtime,n_save_steps),N)
+        # output = np.zeros((n_save_steps*N, 6))
 
-        for i in range(N):
-            df_i = self.simulate_trajectory()
-            lower = i*n_save_steps
-            upper = (i+1)*n_save_steps
-            #output.iloc[lower:upper,:] = df_i
-            output.loc[lower:upper,'glucose_t'] = df_i.glucose_t
-            output.loc[lower:upper,'glucose_t_obs'] = df_i.glucose_t_obs
-            output.loc[lower:upper,'obs'] = df_i.obs
-            output.loc[lower:upper,'insulin_t'] = df_i.insulin_t
-            output.loc[lower:upper,'dextrose_t'] = df_i.dextrose_t 
+        output = _simulate(N,self.sigma_m,self.seed)
 
+        # for i in range(N):
+        #     output_i = self.simulate_trajectory()
+        #     lower = i*n_save_steps
+        #     upper = (i+1)*n_save_steps
+        #     output[lower:upper,:] = output_i
 
-        return output
-
-
-
-# function simulate_ensemble(N::Int,error)
-#     # generate
-#     df_sim = DataFrame(id=Int64[],
-#                         t=Float64[],
-#                         obs=Int64[],
-#                         x_true=Float64[],
-#                         x=Float64[],
-#                         m=Float64[],
-#                         g=Float64[])
-#     for i in 1:N
-#         df_i = simulate_glucose_icu(g,next_obs,error);
-#         df_i.id = repeat([i],size(df_i,1))
-#         append!(df_sim,df_i)
-#     end
-#     df_sim            
-# end
+        df = pd.DataFrame(output,columns=self.columns)
+        df['id'] = np.repeat(range(0,N),n_save_steps)
+        df['obs'] = (df.obs == 1.0)
+        return df
